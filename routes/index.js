dd52e08fdc9b4a9ee6bb28351edadd1bbc8f8880
avaneeshdayadav <avaneeshdyadav@gmail.com>
2023-08-19 17:44:17 +0530

    Testing .yaml file commit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const { handleUserSignUp, handleUserLogin } = require('../controllers/user');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json({ "status": "Up and running"});
+  res.json({ "status": "Up and running on port 3000"});
 });
 
 router.post('/signup', handleUserSignUp);
