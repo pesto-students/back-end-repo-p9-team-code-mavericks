b9570284fe5f42a5d80d99fa01650a09b1fdb5a9
Sanjai Sakthivel <42275772+sanjaisak@users.noreply.github.com>
2023-08-21 19:03:34 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const { handleUserSignUp, handleUserLogin } = require('../controllers/user');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json({ "status": "Last Up and running for ci/cd testing."});
+  res.json({ "status": "running for ci/cd testing."});
 });
 
 router.post('/signup', handleUserSignUp);
